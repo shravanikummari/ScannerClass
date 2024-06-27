9de913522b74051501264358c86a6d76238681c4
shravanikummari <shravanikummari1234@gmail.com>
2024-06-27 01:55:09 -0700

    added scanner Multiplication Class

---
+++ src/Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1=in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2=in.nextInt();
+        System.out.println("Multiplication: "+(num1*num2));
+
+    }
+
+} 
+ 
+
+ 
